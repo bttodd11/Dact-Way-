cec523c7ea5b154dfbe9ed8bd2d8babb9ecc7809
bttodd11 <btodd@Brians-MacBook-Pro.local>
2023-07-05 23:49:34 -0500

    Small config changes

--- dact-way/src/App.js
+++ dact-way/src/App.js
@@ -1,7 +1,13 @@
 import Preloader from "./components/preloader/preloader";
 import Nav from "./components/nav/nav";
 import About from "./components/about/about";
-import Stats from "./components/stats/stats"
+import Stats from "./components/stats/stats";
+import Carousel from "./components/carousel/carousel";
+import CarouselIntro from "./components/carouselIntro/carouselIntro";
+import Services from "./components/services/services";
+import Testimonies from "./components/testimonies/testimonies";
+import Contact from "./components/contact/contact";
+import CarouselMid from "./components/carouselMid/carouselMid";
 import { BrowserRouter } from "react-router-dom";
 import video from "../src/components/video/dactWayVideo.mp4";
 import "./App.css";
@@ -19,14 +25,20 @@ function App() {
           <Preloader />
         </div>
         <div className="mainSection">
-                    <Nav />
+          <Nav />
           <div className="videoContainer">
             <video autoPlay muted loop playsInline className="hero">
               <source src={video} type="video/mp4" />
             </video>
           </div>
-      <About />
-      <Stats />
+          <About />
+          <CarouselIntro />
+          <Services />
+          <CarouselMid />
+          <Stats />
+          <Testimonies />
+          <Contact />
+          <Carousel />
         </div>
       </div>
     </BrowserRouter>
