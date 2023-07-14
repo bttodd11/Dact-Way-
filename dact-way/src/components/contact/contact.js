c745decc4f0cef539c5b04c797faf0070c3d2c48
bttodd11 <bttodd@icloud.com>
2023-07-14 11:20:42 -0500

    Updated some links

--- dact-way/src/components/contact/contact.js
+++ dact-way/src/components/contact/contact.js
@@ -14,9 +14,9 @@ let contact = () => {
       <Card className="contactCard">
       <Card.Body>
       <Card.Text className="statText">
-          <img src={facebookIcon} alt="Facebook Icon" className="contactIcon" />
-          <img src={instagramIcon} alt="Instagram Icon" className="contactIcon" />
-          <img src={emailIcon} alt="Email Icon" className="contactIcon" />
+          <a href="https://www.facebook.com/dactway"><img src={facebookIcon} alt="Facebook Icon" className="contactIcon" /></a>
+          <a href="https://www.instagram.com/dactway/"><img src={instagramIcon} alt="Instagram Icon" className="contactIcon" /></a>
+          <a href="mailto:Dactwayservices@gmail.com"><img src={emailIcon} alt="Email Icon" className="contactIcon" /></a>
         </Card.Text>
       </Card.Body>
     </Card>
